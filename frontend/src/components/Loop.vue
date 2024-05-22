4338506935dd70b10c80ac1d0be03e24877b4a6b
lislis <mail@lislis.de>
2024-05-22 17:15:29 +0200

    pull level data into data

--- frontend/src/components/Loop.vue
+++ frontend/src/components/Loop.vue
@@ -4,15 +4,15 @@
  import { reactive, ref, watch } from 'vue'
  import { storeToRefs } from 'pinia'
  import { useClickStore } from '@/stores/click'
+ import { levels } from '@/data/game'
 
  const store = useClickStore();
- const { getGame } = storeToRefs(store)
+// const { getGame } = storeToRefs(store)
 
  import Rocket from '@/components/Rocket.vue'
  import Moon from '@/components/Moon.vue'
 
  const width = (window.innerWidth);
-
 
  function distanceBetweenTwoPoints(p1, p2) {
      const a = p1.x - p2.x;
@@ -21,43 +21,28 @@
      return Math.hypot(a, b);
  }
 
- const position = reactive({ x: 80, y: 150 })
- const moon = reactive({ x: width - 70, y: 60, scale: 0.1})
+ const level = ref(1);
+ let state = reactive(levels[level.value]);
+ //const position = reactive(levels[level.value].rocket)
+ //const moon = reactive(levels[level.value].moon)
  const distance = ref(0);
+ //const distance2Match = ref(levels[level.value].distance)
 
- function resetRocket() {
-     position.x = 80;
-     position.y = 150;
- }
  function levelup() {
-     resetRocket();
-     getGame.value.level += 1;
-     setMoon(getGame.value.level);
- }
- function setMoon(level) {
-     console.log('geing here', level)
-     switch(level) {
-         case 2:
-             moon.scale = 0.2;
-             break;
-         case 3:
-             moon.scale = 0.3;
-             break;
-         case 4:
-             moon.scale = 0.4;
-             break;
-     }
+     level.value = level.value + 1;
+     state = reactive(levels[level.value]);
+     //position.value = levels[level.value].rocket;
+     //moon.value = levels[level.value].moon;
+     //distance2Match = levels[level.value].distance
  }
 
  onTick((dt) => {
-     //console.log('distance ', distanceBetweenTwoPoints(position, moon))
-     distance.value = distanceBetweenTwoPoints(position, moon);
+     distance.value = distanceBetweenTwoPoints(state.rocket, state.moon);
  })
 
  whenever(
-     () => distance.value < 75,
+     () => distance.value < state.distance,
      () => {
-         console.log(distance.value, getGame.level)
          levelup()
      }
  )
@@ -66,11 +51,10 @@
 <template>
     <container>
         <text :x="20" :y="20"
-              :style="{ fill: 'white' }">To the Moon!</text>
+              :style="{ fill: 'white' }">{{state.blurp}}</text>
 
-        <Moon v-model:x="moon.x" v-model:y="moon.y" v-model:scale="moon.scale" />
+        <Moon v-model:x="state.moon.x" v-model:y="state.moon.y" v-model:scale="state.moon.scale" />
 
-        <Rocket v-model:x="position.x" v-model:y="position.y"
-                @die="gameover = true" />
+        <Rocket v-model:x="state.rocket.x" v-model:y="state.rocket.y" />
     </container>
 </template>
