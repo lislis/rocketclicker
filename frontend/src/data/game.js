e925a23e553b63c3721c78e31f13af467a61b155
lislis <mail@lislis.de>
2024-05-22 17:17:01 +0200

    tweak moon position

--- frontend/src/data/game.js
+++ frontend/src/data/game.js
@@ -1,25 +1,25 @@
 export const levels = {
   1: {
     rocket: { x: 80, y: 150 },
-    moon: { x: 500, y: 60, scale: 0.1},
+    moon: { x: 700, y: 60, scale: 0.1},
     distance: 75.0,
     blurp: "To the Moon!"
   },
   2: {
     rocket: { x: 80, y: 150 },
-    moon: { x: 500, y: 80, scale: 0.2},
+    moon: { x: 700, y: 80, scale: 0.2},
     distance: 100.0,
     blurp: "Up and to the right!"
   },
   3: {
     rocket: { x: 80, y: 150 },
-    moon: { x: 500, y: 100, scale: 0.3},
+    moon: { x: 700, y: 100, scale: 0.3},
     distance: 120.0,
     blurp: "To, to, to, to the moon!"
   },
   4: {
     rocket: { x: 80, y: 150 },
-    moon: { x: 500, y: 120, scale: 0.4},
+    moon: { x: 700, y: 120, scale: 0.4},
     distance: 150.0,
     blurp: "Something, something moon!"
   }
