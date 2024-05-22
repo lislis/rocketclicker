b7f281acce9eb3473fdcb76e3e525845bb033366
lislis <mail@lislis.de>
2024-05-22 17:18:04 +0200

    rm unused code

--- frontend/src/components/Loop.vue
+++ frontend/src/components/Loop.vue
@@ -2,17 +2,10 @@
  import { useEventListener, whenever } from '@vueuse/core'
  import { Application, Loader, onTick } from "vue3-pixi";
  import { reactive, ref, watch } from 'vue'
- import { storeToRefs } from 'pinia'
- import { useClickStore } from '@/stores/click'
  import { levels } from '@/data/game'
-
- const store = useClickStore();
-// const { getGame } = storeToRefs(store)
 
  import Rocket from '@/components/Rocket.vue'
  import Moon from '@/components/Moon.vue'
-
- const width = (window.innerWidth);
 
  function distanceBetweenTwoPoints(p1, p2) {
      const a = p1.x - p2.x;
@@ -23,17 +16,11 @@
 
  const level = ref(1);
  let state = reactive(levels[level.value]);
- //const position = reactive(levels[level.value].rocket)
- //const moon = reactive(levels[level.value].moon)
  const distance = ref(0);
- //const distance2Match = ref(levels[level.value].distance)
 
  function levelup() {
      level.value = level.value + 1;
      state = reactive(levels[level.value]);
-     //position.value = levels[level.value].rocket;
-     //moon.value = levels[level.value].moon;
-     //distance2Match = levels[level.value].distance
  }
 
  onTick((dt) => {
