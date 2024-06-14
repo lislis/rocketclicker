f435c5c563f5a9e8acda93383d79c2dec211ad88
lislis <mail@lislis.de>
2024-06-14 16:45:22 +0200

    draw minimal version of candlestick

--- frontend/src/data/game.js
+++ frontend/src/data/game.js
@@ -1,14 +1,14 @@
 export const levels = {
   1: {
-    rocket: { x: 80, y: 50 },
+    rocket: { x: 0, y: 50 },
     moon: { x: 170, y: 110, scale: 0.1},
     distance: 75.0,
     bg: {x: 0, y: 0, scale: 1, width: 2000 }, //1 2000
     blurp: "To the Moon!",
-    skyline: { x: 0, y: 309, width: 3905, scale: 0.9 }
+    skyline: { x: 0, y: 409, width: 3905, scale: 0.9 }
   },
   2: {
-    rocket: { x: 80, y: 350 },
+    rocket: { x: 0, y: 350 },
     moon: { x: 200, y: 160, scale: 0.3},
     distance: 100.0,
     bg: {x: 0, y: -30, scale: 1.2, width: 1666 },
@@ -16,23 +16,23 @@ export const levels = {
     skyline: { x: 0, y: 629, width: 1415, scale: 0.34 }
   },
   3: {
-    rocket: { x: 80, y: 350 },
+    rocket: { x: 0, y: 350 },
     moon: { x: 220, y: 260, scale: 0.5},
     distance: 180.0,
     bg: {x: 0, y: -20, scale: 1.35, width: 1481 },
     blurp: "To, to, to, to the moon!",
     skyline: { x: 0, y: 2099, width: 815, scale: 0.2 }
   },
+  // 4: {
+  //   rocket: { x: 0, y: 350 },
+  //   moon: { x: 300, y: 220, scale: 0.7},
+  //   distance: 200.0,
+  //   bg: {x: 0, y: -40, scale: 1.5, width: 1333 },
+  //   blurp: "Something, something moon!",
+  //   skyline: {x: 0, y: 2000, width: 4405, scale: 0.2 }
+  // },
   4: {
-    rocket: { x: 80, y: 350 },
-    moon: { x: 300, y: 220, scale: 0.7},
-    distance: 200.0,
-    bg: {x: 0, y: -40, scale: 1.5, width: 1333 },
-    blurp: "Something, something moon!",
-    skyline: {x: 0, y: 2000, width: 4405, scale: 0.2 }
-  },
-  5: {
-    rocket: { x: 80, y: 350 },
+    rocket: { x: 0, y: 350 },
     moon: { x: 350, y: 280, scale: 0.99},
     distance: 300.0,
     bg: {x: 0, y: -40, scale: 1.5, width: 1333 },
